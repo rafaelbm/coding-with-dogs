421f3cc812c3e1c274eb99d252628cb0f21f8881
rafaelbm <5865557+rafaelbm@users.noreply.github.com>
2018-05-23 23:54:55 -0300

    Add custom css to table

--- conf.py
+++ conf.py
@@ -76,6 +76,9 @@ pygments_style = 'sphinx'
 #
 html_theme = 'sphinx_rtd_theme'
 
+def setup(app):
+    app.add_stylesheet('css/custom.css')
+
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
